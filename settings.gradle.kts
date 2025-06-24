606ffafc5dff9941e95fa1ff7ab8f01431369c20
zhouzixuan <zhouzixuan@wangtengtech.com>
2025-06-24 09:36:48 +0800

    仓库修改为阿里云

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,8 +14,16 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        // 使用阿里云的镜像作为主仓库，速度会快很多
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+
+        // 保留官方仓库作为备用
         google()
         mavenCentral()
+
+        // 如果有其他库需要，可以保留
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
